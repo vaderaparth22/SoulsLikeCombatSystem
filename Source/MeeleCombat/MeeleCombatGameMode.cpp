6e17fda40fecbff7a75a5729a91e7685587a7dd4
Parth <vaderaparth22@gmail.com>
2022-10-28 00:51:04 -0400

    Added Character and Sword

--- Source/MeeleCombat/MeeleCombatGameMode.cpp
+++ Source/MeeleCombat/MeeleCombatGameMode.cpp
@@ -7,7 +7,7 @@
 AMeeleCombatGameMode::AMeeleCombatGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/CombatSystem/Blueprints/BP_CombatCharacter"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
